a35dbc3729e6d8e6ecc9d0eba436d7002344e294
byhuaji <127609291+byhuaji@users.noreply.github.com>
2023-04-08 21:43:20 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="initial-scale=1, maximum-scale=3, minimum-scale=1, user-scalable=no">
+<title>hrhr鉴赏馆</title>
+<style type="text/css">
+body,td,th {
+	text-align: center;
+	font-family: "微软雅黑", "Droid Sans Fallback", "黑体", sans-serif;
+}
+a:visited {
+	color: #FFFFFF;
+}
+</style>
+</head>
+
+<body bgcolor="#FEB2AF" text="#000000" link="#FF3700">
+<h1 style="text-align: center">hrhr鉴赏馆</h1>
+<hr />
+<p style="text-align: left; color: #000000;"><strong>优秀作品</strong></p>
+<blockquote>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=54&id=340&pathid=0">武器A(来自某远古时期的洗脑视频)</a></p>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=2133&pathid=0">金属hrhr</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1232&pathid=0">废狱hrhr</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1037&pathid=0">hrhr zone</a></p>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1023&pathid=0">hrhr so happy</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1034&pathid=0">陆行鸟hrhr</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1033&pathid=0">孕腹飞翔 ~ Lunamoto Moo</a></p>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1036&pathid=0">危险的hrhr</a>
+  <p style="text-align: left"><a href="http://lfzmjp.free.yzy1.top/video/wuqiB.mp4">武器B</a>
+  <p style="text-align: left"><a href="http://lfzmjp.free.yzy1.top/video/hrhryjs.mp4">hrhr与鉴赏</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=2169&pathid=0">武器T(pv修正)</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=54&id=2175&pathid=0">武器M</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=54&id=2176&pathid=0">武器∞</a>
+  <p style="text-align: left"><a href="http://lfzmjp.free.yzy1.top/video/smhrhr.mp4">圣母hrhr</a>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=2305&pathid=0">HRHR Reflection</a></p>
+  <p style="text-align: left">&nbsp;</p>
+</blockquote>
+<p style="text-align: left"><strong>屑作</strong></p>
+<blockquote>
+  <p style="text-align: left"><a href="http://m.ailicili.tv/e/DownSys/play/?classid=54&id=1974&pathid=0">hrhr头旋转一分钟</a></p>
+  <p style="text-align: left">&nbsp;</p>
+</blockquote>
+<p style="text-align: left"><strong>AV10492</strong></p>
+<blockquote>
+  <p style="text-align: left">P1<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=0">死了</a>
+  <p style="text-align: left">P2<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=1">较猎奇</a>
+  <p style="text-align: left">P3<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=2">全员猎奇</a>
+  <p style="text-align: left">P4<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=3">惊悚式猎奇</a>
+  <p style="text-align: left">P5<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=4">轻度精神猎奇</a>
+  <p style="text-align: left">P6<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=5">重度精神猎奇</a>
+  <p style="text-align: left">P7<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=6">给人留下阴影的惊悚式猎奇</a>
+  <p style="text-align: left">P8<a href="http://m.ailicili.tv/e/DownSys/play/?classid=70&id=1458&pathid=7">令人反感,无法让人接受的终极猎奇</a></p>
+  <hr />
+  <h6 style="text-align: center">由于服务器原因，站长会尽量把视频链接作为站外链接，实在找不到的视频才会放在内链里，内链视频加载较慢，建议使用加速器 </h6>
+  <p style="text-align: center"><a href="http://lfzmjp.free.yzy1.top/pinglun/">评论区</a></p>
+
